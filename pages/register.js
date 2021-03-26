4dd1476de1fce5e53069b28bcde9227811ede548
Prarthana Rode <pvrode88@gmail.com>
2021-03-26 23:11:46 +0530

    second

--- pages/register.js
+++ pages/register.js
@@ -10,13 +10,15 @@ const Register = (props) => {
     const [confirmPwd,setConfirmPwd]=useState("");
     const [errorMsg, setMsg]=useState("");
     const [myfile, setFile]=useState("");
-    const [profile,setProfile]=useState(props.profile);
+    const [profile,setProfile]=useState("");
+    const [userEmail,setUserEmail]=useState("");
 
     const router=useRouter();
 
     useEffect(()=>{
         var user =auth.currentUser;
         if(user){
+            setUserEmail(user.email);
             var url="profiles/"+user.email+"/"+"img.png";
             var refstorage= storage.ref();
             refstorage.child(url).getDownloadURL().then((imgurl)=>{
@@ -29,21 +31,19 @@ const Register = (props) => {
     function registerUser(event){
 
         event.preventDefault();
-        var emailId=email;
+      /*   var emailId=email;
         var password=pwd;
         var confirmPassword=confirmPwd;
-        var myfile1=myfile;
-        debugger;
-        if(confirmPassword==password){
-            if(password.length>5){
-                auth.createUserWithEmailAndPassword(emailId,password).then((cred)=>{
+        var myfile1=myfile; */
+        if(confirmPwd==pwd){
+            if(pwd.length>5){
+                auth.createUserWithEmailAndPassword(email,pwd).then((cred)=>{
                     setMsg("User registered");
                 }).catch(error=>{
                     setMsg(error.message);
                 });
-               var storageRef= storage.ref("profiles/"+emailId + "/"+ "img.png");
-               storageRef.put(myfile1);
-               
+               var storageRef= storage.ref("profiles/"+email + "/"+ "img.png");
+               storageRef.put(myfile);
             }
             else{
                 setMsg("Password should have atleast 6 characters");
@@ -63,27 +63,23 @@ const Register = (props) => {
             setMsg(error.message);
         })
     }
-
     return ( 
         <div>
-           <div className="header" >Registration Form <img src={profile} style={{height:"30px", width:"50px",float:"left", marginTop: "10px"}}></img> <button onClick={signOut} className="btnSignOut" >SignOut</button></div  >
+           <div className="header" ><span className="userInfo" style={{float:"left"}}><img src={profile} style={{height:"30px", width:"50px", marginTop: "10px"}}></img> <span>{userEmail}</span></span>Registration Form  <button onClick={signOut} className="btnSignOut" >Sign Out</button></div  >
             <form className="formCss" onSubmit={registerUser}>
-               
                     <label htmlFor="name" >Email</label>
                     <input required type="email" name="email" value={email} onChange={(event)=>{setEmail(event.target.value)}}/><br/>
                     <label  htmlFor="pwd" >Password</label>
                     <input required type="password" value={pwd} name="pwd" onChange={(event)=>{setPwd(event.target.value)}} /><br/>
                     <label  htmlFor="pwd" >Password</label>
                     <input required type="password" value={confirmPwd} name="confirmPwd" onChange={(event)=>{setConfirmPwd(event.target.value)}} /><br/>
+                    <label  htmlFor="pwd" >Profile photo:     </label>
                     <input  required  type="file" onChange={(event)=>{ setFile(event.target.files[0])}} name="file"></input>
                 <div style={{textAlign:"center"}} ><input type="submit" value="Submit" /></div>
                 <div style={{color:"red"}}>{errorMsg}</div>
              </form>
             
         </div> 
-        
     );
 }
- 
-
-export default Register;
+ export default Register;
